4c6c3306257abaada8679479b762b85307fde1b5
Julie Crandall <10697751@my.uvu.edu>
2019-08-30 15:17:56 -0600

    Testing out tutorial code. Defined meanings in word. Fixed bugs I somehow created...

---
+++ Happy tryout/Assets/Scenes/IfStatements.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+	private float cocoaTemperature = 85.0f;
+	private float hotlimitTemperature = 70.0f;
+	private float coldlimitTemperature = 40.0f;
+	
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+			TemperatureTest();
+
+		cocoaTemperature -= Time.deltaTime * 5f;
+	}
+
+	void TemperatureTest()
+	{
+		if (cocoaTemperature > hotlimitTemperature)
+		{
+			print("Cocoa is extremely hot.");
+		}
+		else if (cocoaTemperature < coldlimitTemperature)
+		{
+			print("Cocoa is chilly.");
+		}
+		else
+		{
+			print("Cocoa is perfect.");
+		}
+	}
+}
