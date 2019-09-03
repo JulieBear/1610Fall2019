bd839c4994f39eabc9de61f5a715d6fffe8c6eee
Julie Crandall <10697751@my.uvu.edu>
2019-09-03 15:21:59 -0600

    In class learning and testing.

---
+++ Happy tryout/Assets/Scenes/Variables.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Variables : MonoBehaviour
+{
+
+	public float floatValue = 5.5f;
+	public int intValue = 20;
+	public string stringValue = "Bob";
+	public int firePower;
+	public UnityEvent Event;
+	private void OnTriggerEnter(Collider other)
+	{
+		print("testing");
+		Event.Invoke();
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
