0304286891cf6c3c82c9c34c1e1910ea28b7a2aa
Julie Crandall <10697751@my.uvu.edu>
2019-09-04 23:52:58 -0600

    Testing triggers and noting what they do for future reference.

---
+++ Happy tryout/Assets/Scenes/Cylinder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cylinder : MonoBehaviour
+{
+    public float floatValue = 12.3f;
+    public int intValue = 47;
+    public string stringValue = "Cheese";
+
+
+    //When it ENTERS sphere, it causes message.
+    public void OnTriggerEnter(Collider other)
+    {
+        print("What does this one do?");
+    }
+
+    //When it EXITS sphere, it causes message.
+    public void OnTriggerExit(Collider other)
+    {
+        print("So, does this make message when it goes through?");
+    }
+
+    //Creates message when hitting sphere AND the message continues if left in sphere. Over and over.
+    public void OnTriggerStay(Collider other)
+    {
+        print("This will be an interesting one.");
+    }
+}
+
+
+
--- Happy tryout/Assets/Scenes/Variables.cs
+++ Happy tryout/Assets/Scenes/Variables.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using UnityEngine.Experimental.UIElements;
 
 public class Variables : MonoBehaviour
@@ -52,4 +53,6 @@ public class Variables : MonoBehaviour
 		print("testing");
 		Event.Invoke();
 	}
+
+	
 }
