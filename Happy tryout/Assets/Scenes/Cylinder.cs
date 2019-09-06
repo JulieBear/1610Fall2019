84cdc7f4329750dab197cfbc43d03fd12f9c5170
Julie Crandall <10697751@my.uvu.edu>
2019-09-05 21:11:06 -0600

    Playing with remaining commands that pop up for trigger and mouse. Many didn't work due to lack of knowledge. It was fun.

--- Happy tryout/Assets/Scenes/Cylinder.cs
+++ Happy tryout/Assets/Scenes/Cylinder.cs
@@ -1,15 +1,20 @@
-﻿using System;
+﻿//Note to Professor (I'll ask you personally as well), I am not quite understanding how 
+//to make certain events happen. Such as that particle explosion. I've tried replicating
+//what you showed us on Tuesday, but no matter how many times I try, it just isn't working.
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.UIElements;
 
 public class Cylinder : MonoBehaviour
 {
     public float floatValue = 12.3f;
     public int intValue = 47;
     public string stringValue = "Cheese";
-
-
+    
+    
+    
     //When it ENTERS sphere, it causes message.
     public void OnTriggerEnter(Collider other)
     {
@@ -22,11 +27,33 @@ public class Cylinder : MonoBehaviour
         print("So, does this make message when it goes through?");
     }
 
+    // Asking for return type, unsure of what that is, ask professor.
+    //public MouseExitWindowEvent()
+    //{
+     //   print("leave and come back");
+   // }
+
     //Creates message when hitting sphere AND the message continues if left in sphere. Over and over.
-    public void OnTriggerStay(Collider other)
+    //public void OnTriggerStay(Collider other)
+    //{
+      //  print("This will be an interesting one.");
+    //}
+
+    
+    // Couldn't get this one to do anything, as I suspected with the particle part.
+    //public void OnParticleTrigger()
+    //{
+      //  print("this might not do anything.");
+    //}
+
+    // So keycode can be used for many different keys.
+    void Update()
     {
-        print("This will be an interesting one.");
+        if (Input.GetKeyDown(KeyCode.R))
+            print("would this do something?");
     }
+    
+    
 }
 
 
