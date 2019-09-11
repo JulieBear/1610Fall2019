3bb9434d8995a14390239cce37842cef085db36d
Julie Crandall <10697751@my.uvu.edu>
2019-09-10 21:24:52 -0600

    Creating assets/functions/etc

---
+++ Happy tryout/Assets/Scripts/FloatValue.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+
+public class FloatValue : ScriptableObject
+{
+    public float value = 1f;
+
+    public void UpdateValue(float amount)
+    {
+        value += amount;
+    }
+    
+}
---
+++ Happy tryout/Assets/Scripts/TriggerEvents.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Script
+{
+    public class TriggerEvents : MonoBehaviour
+    {
+        public UnityEvent triggerEvent;
+            
+        // W: ontrigger
+        private void OnTriggerEnter(Collider other)
+        {
+            triggerEvent.Invoke();
+        }
+    }
+}
