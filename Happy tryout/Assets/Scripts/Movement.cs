c4740349f8e1a5c205b5d3eb7aef1b442d7c622e
Julie Crandall <10697751@my.uvu.edu>
2019-09-17 15:54:37 -0600

    In class character controller.

---
+++ Happy tryout/Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+	public float speed = 1f;
+	public float gravity = -3f;
+	public float jumpSpeed = 100f;
+	
+	private Vector3 position;
+	public CharacterController controller;
+	void Update () 
+	{
+		//transform.Translate(position*Time.deltaTime);
+		position.x = speed * Input.GetAxis("Horizontal");
+		//position.y += gravity;
+
+		// -- Runner
+		//position.x = speed;
+
+		if (!controller.isGrounded)
+		{
+			position.y += gravity;
+		}
+		else
+		{
+			position.y = 0f;
+		}
+		
+		if (Input.GetButtonDown("Jump"))
+		{
+			position.y = jumpSpeed;
+		}
+		
+		controller.Move(position*Time.deltaTime);
+	}
+}
