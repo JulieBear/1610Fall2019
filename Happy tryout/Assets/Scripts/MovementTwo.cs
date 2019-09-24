0945d2e5c6dbbcc573d300946b9efdab53618b1b
Julie Crandall <10697751@my.uvu.edu>
2019-09-23 22:29:57 -0600

    Watching videos. Hit a rut, feel like I'm not learning anything new on me own. Learning a lot be checking "vocab" (so to speak) from typing random script.

--- Happy tryout/Assets/Scripts/MovementTwo.cs
+++ Happy tryout/Assets/Scripts/MovementTwo.cs
@@ -1,23 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(CharacterController))]
 public class MovementTwo : MonoBehaviour
 {
+	public UnityEvent testEvent;
 	public CharacterController controller;
 	public float moveSpeed = 10f, gravity = 9.81f, jumpSpeed = 30f;
 	private int jumpCount;
 	public int jumpCountMax = 2;
 	private Vector3 position;
-	
+
+	private void OnMouseDown()
+	{
+		testEvent.Invoke();
+	}
+
 	void Start ()
 	{
 		controller = GetComponent<CharacterController>();
 	}
 	
+
 	void Update ()
 	{
-		position.x = moveSpeed*Input.GetAxis("Vertical");
-		position.z = moveSpeed * Input.GetAxis("Horizontal");
+		position.x = moveSpeed*Input.GetAxis("Horizontal");
 		position.y -= gravity;
 
 		if (controller.isGrounded)
