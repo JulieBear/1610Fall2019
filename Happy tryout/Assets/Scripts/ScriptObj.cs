bd0ecb1faae397ba97460be4cdc13067f118f476
Julie Crandall <10697751@my.uvu.edu>
2019-09-27 20:01:42 -0600

    Made a new asset.

---
+++ Happy tryout/Assets/Scripts/ScriptObj.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ScriptObj : ScriptableObject
+{
+	
+	public float value = 1f;
+
+	public void UpdateValue(float amount)
+	{
+		value += amount;
+	}
+
+	public new string name;
+	public string description;
+
+	public int health;
+	public int attackPower;
+	public int luck;
+	public int charm;
+	public int navigationSkills;
+
+}
