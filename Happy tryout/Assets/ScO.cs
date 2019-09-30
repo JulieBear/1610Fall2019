5d9de1c60d5193563964b4b6bd6301e81e03d4e8
Julie Crandall <10697751@my.uvu.edu>
2019-09-29 23:53:22 -0600

    Putting things I've learned together.

---
+++ Happy tryout/Assets/ScO.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class ScO : ScriptableObject
+{
+	public new string name;
+
+	public int health;
+	public int magic;
+	public int mysticAmmo;
+	public int luck;
+	public int navigationSkills;
+}
