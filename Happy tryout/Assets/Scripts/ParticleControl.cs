186a84665436b18347f64d1812f71de9b2989114
Julie Crandall <10697751@my.uvu.edu>
2019-09-30 23:33:48 -0600

    Better understanding of ontriggerenter. yay.

--- Happy tryout/Assets/Scripts/ParticleControl.cs
+++ Happy tryout/Assets/Scripts/ParticleControl.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ParticleControl : MonoBehaviour
 {
 
+	public UnityEvent test;
 	public ParticleSystem particles;
 	
-	private void OnMouseDown()
+	//private void OnMouseEnter()
+	//{
+	//	particles.Emit(100);
+	//}
+
+	private void OnTriggerEnter(Collider other)
 	{
+		test.Invoke();
 		particles.Emit(100);
 	}
 	
