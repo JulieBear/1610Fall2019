3a01e7ac184b9bad7fb528fea74427b2f673f5fe
Julie Crandall <10697751@my.uvu.edu>
2019-10-31 15:55:09 -0600

    In class, AI.

---
+++ Happy tryout/Assets/Scripts/AIScripts/AgentHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AgentHandler : MonoBehaviour
+{
+	private Transform currentDestination;
+	private NavMeshAgent agent;
+	private GameObject startObj;
+	public Transform destinationObj;
+	
+	
+	void Start ()
+	{
+		startObj = new GameObject();
+		startObj.transform.position = transform.position;
+		currentDestination = transform;
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		currentDestination = destinationObj;
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		currentDestination = startObj.transform;
+	}
+
+	void Update ()
+	{
+		agent.destination = destinationObj.position;
+	}
+}
---
+++ Happy tryout/Assets/Scripts/AIScripts/AgentHandler2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AgentHandler2 : MonoBehaviour
+{
+    private Vector3 currentDestin;
+    private NavMeshAgent agent;
+    private Vector3 startPoint;
+    public Transform destinationObj;
+	
+	
+    void Start ()
+    {
+        startPoint = transform.position;
+        currentDestin = startPoint;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        currentDestin = destinationObj.position;
+    }
+
+    //--return back to locations
+    private void OnTriggerExit(Collider other)
+    {
+        currentDestin = startPoint;
+    }
+
+    void Update ()
+    {
+        agent.destination = currentDestin;
+    }
+}
