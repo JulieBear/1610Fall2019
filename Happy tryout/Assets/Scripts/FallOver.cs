afbb7a151395f09e68f1f06f3f5972a80c14ba5d
Julie Crandall <10697751@my.uvu.edu>
2019-11-03 23:59:32 -0700

    Worked with a fall over script through Unity tutorial. Sort of worked, needs some improvements on my part.

---
+++ Happy tryout/Assets/Scripts/FallOver.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+ 
+[RequireComponent(typeof(Rigidbody))] 
+public class FallOver : MonoBehaviour 
+{
+ 
+    private Rigidbody rigBody;
+    public float mDistance = 3f;
+    public Transform thePlayer; 
+    
+    void Start () 
+    {
+        rigBody = GetComponent<Rigidbody> ();
+        rigBody.useGravity = false;
+    }
+ 
+    void Update()
+    {
+        if (Vector3.Distance (thePlayer.position, transform.position) < mDistance) 
+        { 
+            rigBody.useGravity = true;
+        }
+    }
+}
