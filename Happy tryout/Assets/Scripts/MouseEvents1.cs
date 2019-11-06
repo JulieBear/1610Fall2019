9c644a53afc9780a895585a1bd47987f583fe978
Julie Crandall <10697751@my.uvu.edu>
2019-11-05 23:06:25 -0700

    In class AI 2.

---
+++ Happy tryout/Assets/Scripts/MouseEvents1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseEvents1 : MonoBehaviour
+{
+	private Camera cam;
+	public Vector3Data data;
+	
+
+	private void Start()
+	{
+		cam = Camera.main;
+	}
+
+	private void OnMouseDown()
+	{
+		var ray = cam.ScreenPointToRay(Input.mousePosition);
+		var hit = ray.GetPoint(10);
+		data.value = hit;
+		Debug.Log(hit);
+	}
+}
