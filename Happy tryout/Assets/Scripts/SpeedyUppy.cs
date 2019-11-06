c3e28be0cfbf6b364c4a0447e8ad098b27a876b3
Julie Crandall <10697751@my.uvu.edu>
2019-11-05 23:51:25 -0700

    Worked on SpeedUp script, didn't work, commented it all out. Claimed I had an error on a line that didn't even exist.

---
+++ Happy tryout/Assets/Scripts/SpeedyUppy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+//using UnityEngine;
+
+//public class SpeedyUppy : MonoBehaviour
+//{
+	//public bool upSpeed = false;
+	//public float quicken;
+	//public float speeder = 100;
+
+	//private void OnTriggerEnter(Collider other)
+	//{
+	//	if (other.CompareTag("SPlayer"))
+		//{
+		//	upSpeed = true;
+		//	Debug.Log("speed up");
+		//	MovementThree theSpeed = other.gameObject.GetComponent<MovementThree>();
+		//	theSpeed.moveSpeed = quicken;
+		//	StartCoroutine(WaitTime(0));
+		//}
+
+		//IEnumerator WaitTime(int r)
+		//{
+		//	yield return new WaitForSeconds(5);
+			//upSpeed = false;
+		//}
+	//}
+//}
+
+
