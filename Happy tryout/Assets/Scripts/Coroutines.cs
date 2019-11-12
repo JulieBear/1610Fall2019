07011cb5f16768247920ce28c4df874ee04d4b17
Julie Crandall <10697751@my.uvu.edu>
2019-11-12 15:23:38 -0700

    In class coroutines.

---
+++ Happy tryout/Assets/Scripts/Coroutines.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Coroutines : MonoBehaviour
+{
+	private int i = 3;
+	private WaitForSeconds wfsObj = new WaitForSeconds(1f);
+	
+	IEnumerator Start()
+	{
+		while (i > 0)
+		{
+			yield return wfsObj;
+			Debug.Log(i);
+			i--;
+		}
+		yield return new WaitForSeconds(1f);
+		Debug.Log("GO!");
+	}
+	
+	
+	
+	
+	//{
+	//	yield return new WaitForSeconds(1f);
+	//	Debug.Log(3);
+	//	yield return new WaitForSeconds(1f);
+	//	Debug.Log(2);
+	//	yield return new WaitForSeconds(1f);
+	//	Debug.Log(1);
+	//	yield return new WaitForSeconds(1f);
+	//	Debug.Log("Go");
+	//}
+}
