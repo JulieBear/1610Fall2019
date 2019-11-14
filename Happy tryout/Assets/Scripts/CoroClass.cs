d85295f98694b3b55e9e29a0b947f6be890a2f3a
Julie Crandall <10697751@my.uvu.edu>
2019-11-14 15:46:12 -0700

    In class coroutine

---
+++ Happy tryout/Assets/Scripts/CoroClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroClass : MonoBehaviour
+
+{
+	public bool canRun = true;
+	public int index = 4;
+	public float seconds = 1f;
+	public UnityEvent test;
+
+	private WaitForSeconds wfsObj2;
+		//public ParticleSystem partPart;
+	
+		
+	IEnumerator OnMouseDown()
+	{
+		while (index > 0)
+		{
+			Debug.Log(index);
+			index--;
+			yield return new WaitForSeconds(1f);
+			//partPart.Emit(20);
+			//test.Invoke();
+		}
+	}
+	
+	
+	
+	
+	
+	//public void Run()
+	//{
+	//	StartCoroutine(OnRun());
+	//}
+
+
+	//IEnumerator onmoused
+	//{
+	//	while (canRun)
+	//	{
+	//		yield return new WaitForSeconds(1f);
+			
+	//	}
+	//}
+}
