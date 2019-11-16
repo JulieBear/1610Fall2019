26e7fcdd98f6b720d700093e032c784d1a446834
Julie Crandall <10697751@my.uvu.edu>
2019-11-15 22:43:11 -0700

    Redid AI configuration to remember how.

---
+++ Happy tryout/Assets/Scripts/Flappy.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(CharacterController))]
+public class Flappy : MonoBehaviour
+{
+    public UnityEvent eve;
+    public CharacterController control;
+    public float moveSpeed = 10f, gravity = 4f, jumpSpeed = 30f;
+   // public int jumpCountMax = 2;
+    private Vector3 pose;
+    private int jumpCount;
+    public static double doSpeedUp;
+
+
+    void Start ()
+    {
+        control = GetComponent<CharacterController>();
+    }
+
+
+    void Update()
+    {
+        pose.x = moveSpeed * Input.GetAxis("Horizontal");
+        //pose.z = moveSpeed * Input.GetAxis("Vertical");
+        pose.y -= gravity;
+        
+
+        if (control.isGrounded)
+        {
+            pose.y = 0;
+            jumpCount = 0;
+        }
+
+
+        control.Move(pose * Time.deltaTime);
+    }
+
+    public static void StopSpeedUp()
+    {
+        throw new System.NotImplementedException();
+    }
+	
+	
+}
