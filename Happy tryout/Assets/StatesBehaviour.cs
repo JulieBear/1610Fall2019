03ffb46a85ba5d345a8f957ce20a8d54269f1cc9
Julie Crandall <10697751@my.uvu.edu>
2019-11-21 22:24:39 -0700

    In class Animation use.

---
+++ Happy tryout/Assets/StatesBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StatesBehaviour : StateMachineBehaviour
+{
+	public UnityEvent StartEnter, StateUpdateEvent, StateExitEvent;
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
+	{
+			StartEnter.Invoke();
+	}
+	
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
+	{
+		StateUpdateEvent.Invoke();
+	}
+	
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
+	{
+		StateExitEvent.Invoke();
+	}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+}
