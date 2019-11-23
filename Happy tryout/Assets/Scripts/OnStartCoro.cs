d5b0db42d7d692bf91f9d772c0ef510c11a97d93
Julie Crandall <10697751@my.uvu.edu>
2019-11-22 23:54:03 -0700

    Played around with scripts to attempt countdown text and rewrote one. Didn't work. Watched unity videos on animation.

---
+++ Happy tryout/Assets/Scripts/OnStartCoro.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnStartCoro : MonoBehaviour
+{
+	public UnityEvent starEvent;
+	public float secs = 1f;
+	public int counts = 3;
+	public bool runs;
+	private WaitForSeconds wsObj;
+	
+	void Start () 
+	{
+		wsObj = new WaitForSeconds(secs);
+	}
+
+	public void CallingCoro()
+	{
+		StartCoroutine(routine: RunCoroutine());
+	}
+
+	IEnumerator RunCoroutine()
+	{
+		starEvent.Invoke();
+
+		while (counts > 0)
+		{
+			yield return wsObj;
+			counts--;
+		}
+	}
+	
+
+	
+}
