5727f66ae0f9a375164d034f8a2960eb7acc6afb
Julie Crandall <10697751@my.uvu.edu>
2019-11-24 23:47:03 -0700

    Figured out what I want to do, planning out game.

---
+++ Happy tryout/Assets/Scripts/Damage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Damage : MonoBehaviour
+{
+	public UnityEvent damage;
+
+	public void OnTriggerStay(Collider other)
+	{
+		damage.Invoke();
+	}
+}
