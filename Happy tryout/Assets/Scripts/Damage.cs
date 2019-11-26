047918a6f3111a815b76e94950498451b373bf02
Julie Crandall <10697751@my.uvu.edu>
2019-11-25 21:05:33 -0700

    Managed to make it wait, but health still goes down on its own after.

--- Happy tryout/Assets/Scripts/Damage.cs
+++ Happy tryout/Assets/Scripts/Damage.cs
@@ -5,28 +5,10 @@ using UnityEngine.Events;
 
 public class Damage : MonoBehaviour
 {
-	public bool running = true;
-	public int dex = 4;
 	public UnityEvent damage;
-	public float time = 3f;
-	private WaitForSeconds secsObj;
-
-	private void Start()
+	
+	public void OnTriggerStay(Collider other)
 	{
-		secsObj = new WaitForSeconds(time);
-	}
-
-	//public void OnTriggerStay(Collider other)
-	//{
-	//	damage.Invoke();
-	//}
-
-	IEnumerator OnTriggerStay(Collider other)
-	{
-		while(dex > 0)
-		{
-			yield return new WaitForSeconds(1f);
-			damage.Invoke();
-		}
+		damage.Invoke();
 	}
 }
---
+++ Happy tryout/Assets/Scripts/TryingThings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TryingThings : MonoBehaviour
+{
+    public bool running = true;
+    public int dex = 1;
+    public UnityEvent damage;
+    public float time = 1f;
+    private WaitForSeconds secsObj;
+
+    private void Start()
+    {
+        secsObj = new WaitForSeconds(time);
+    }
+
+    //public void OnTriggerStay(Collider other)
+    //{
+    //	damage.Invoke();
+    //}
+
+    IEnumerator OnTriggerStay(Collider other)
+    {
+        while(dex > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            damage.Invoke();
+            dex--;
+        }
+        
+    }
+}
