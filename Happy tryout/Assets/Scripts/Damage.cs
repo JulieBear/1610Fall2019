c2e65a6bc2cbdfb5aeea7da0959b548433c03b6b
Julie Crandall <10697751@my.uvu.edu>
2019-11-25 20:39:05 -0700

    Half way figured out countdown, made something super interesting actually!

--- Happy tryout/Assets/Scripts/Damage.cs
+++ Happy tryout/Assets/Scripts/Damage.cs
@@ -1,13 +1,32 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Damage : MonoBehaviour
 {
+	public bool running = true;
+	public int dex = 4;
 	public UnityEvent damage;
+	public float time = 3f;
+	private WaitForSeconds secsObj;
 
-	public void OnTriggerStay(Collider other)
+	private void Start()
 	{
-		damage.Invoke();
+		secsObj = new WaitForSeconds(time);
+	}
+
+	//public void OnTriggerStay(Collider other)
+	//{
+	//	damage.Invoke();
+	//}
+
+	IEnumerator OnTriggerStay(Collider other)
+	{
+		while(dex > 0)
+		{
+			yield return new WaitForSeconds(1f);
+			damage.Invoke();
+		}
 	}
 }
