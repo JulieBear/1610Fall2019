06e15e9135ef3e2808e2736d5fa1db255a5dc54f
Julie Crandall <10697751@my.uvu.edu>
2020-01-21 20:21:15 -0700

    Redid movement script. Attempted to configure gravity to how I like it, didn't work out.

---
+++ SpringSemester/Assets/Scripts/Control.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(CharacterController))]
+public class Control : MonoBehaviour
+{
+
+	public UnityEvent events;
+	public CharacterController conTroll;
+	public float moveTime = 10f, jumpSpe = 20f;
+	public float gravity = 3f;
+	public int jumpMax = 2;
+	public static double spedUp;
+	private Vector3 posit;
+	private int jumpCou;
+	
+	
+	void Start ()
+	{
+		conTroll = GetComponent<CharacterController>();
+	}
+	
+
+	void Update ()
+	{
+		posit.x = moveTime * Input.GetAxis("Horizontal");
+		posit.y -= gravity;
+
+		if (conTroll.isGrounded)
+		{
+			posit.y = 0;
+			jumpCou = 0;
+		}
+
+		if (Input.GetButtonDown("Jump") && jumpCou < jumpMax)
+		{
+			jumpCou++;
+			posit.y = jumpSpe;
+		}
+
+		conTroll.Move(posit * Time.deltaTime);
+	}
+
+}
---
+++ SpringSemester/Assets/Scripts/Gravity.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+
+	public float grav = 3f;
+	private Vector3 move;
+
+	void Start()
+	{
+
+	}
+
+
+	void Update()
+	{
+		move.y = grav;
+	}
+}
