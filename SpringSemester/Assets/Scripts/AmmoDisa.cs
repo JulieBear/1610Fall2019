281486ca86257edcb4aaa1d6306fc50f25d8b6a0
Julie Crandall <10697751@my.uvu.edu>
2020-01-24 23:11:15 -0700

    Created a pick up/destroy tool again.

---
+++ SpringSemester/Assets/Scripts/AmmoDisa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class AmmoDisa : MonoBehaviour {
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PickUp"))
+        {
+            Destroy((other.gameObject));
+        }
+    }
+}
