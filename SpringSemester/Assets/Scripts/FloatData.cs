ff32d384ca8fe6fce36c663db798944c87ac10cf
Julie Crandall <10697751@my.uvu.edu>
2020-01-26 23:16:39 -0700

    Mouse events, fixing my scriptable object.

--- SpringSemester/Assets/Scripts/FloatData.cs
+++ SpringSemester/Assets/Scripts/FloatData.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using UnityEngine;
 
-public class FloatData : MonoBehaviour
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
 {
 
 	public float minVal = 0;
---
+++ SpringSemester/Assets/Scripts/MouseEventsRe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseEventsRe : MonoBehaviour
+{
+
+	public UnityEvent mouseEventRe;
+
+	private void OnMouseDown()
+	{
+		mouseEventRe.Invoke();
+	}
+}
