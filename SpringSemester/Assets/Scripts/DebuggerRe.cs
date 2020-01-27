ca2f1197fdb6b54463b84e4478d549439689466a
Julie Crandall <10697751@my.uvu.edu>
2020-01-26 23:19:59 -0700

    Created my debugger.

---
+++ SpringSemester/Assets/Scripts/DebuggerRe.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class DebuggerRe : ScriptableObject {
+
+	public void TheDebugger(string info)
+	{
+		Debug.Log(info);
+	}
+
+	public void TheDebugger()
+	{
+		Debug.Log(name);
+	}
+}
