72f716c0e4ea6629189e1ebccb93ba4eaa758cc4
Julie Crandall <10697751@my.uvu.edu>
2020-01-27 23:46:02 -0700

    Redoing enum switch.

---
+++ SpringSemester/Assets/Scripts/EnumSwitchRe.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnumSwitchRe : MonoBehaviour
+{
+
+    public enum Says
+    {
+        High,
+        Squat,
+    }
+
+    public Says currState;
+
+    public UnityEvent Onmousedown, Ontriggerexit;
+
+    private void Update()
+    {
+        switch (currState)
+        {
+              case Says.High:
+                  Onmousedown.Invoke();
+                  Debug.Log("practice");
+                  break;
+              case Says.Squat:
+                  Ontriggerexit.Invoke();
+                  Debug.Log("practice 2");
+                  break;
+        }
+    }
+}  
