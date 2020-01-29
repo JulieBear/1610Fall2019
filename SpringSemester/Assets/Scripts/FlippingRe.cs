23b283dc67e89f5b1628edd76059f563198bf52f
Julie Crandall <10697751@my.uvu.edu>
2020-01-28 23:32:07 -0700

    Recreation of flip and vector3data scripts.

---
+++ SpringSemester/Assets/Scripts/FlippingRe.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Boo.Lang;
+using UnityEngine;
+
+public class FlippingRe : MonoBehaviour
+{
+	public VectorThreeD threeD;
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			transform.rotation = Quaternion.Euler(0,180,0);
+			threeD.val.x = -1;
+		}
+
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			transform.rotation = Quaternion.Euler(0,0,0);
+			threeD.val.x = 1;
+		}
+	}
+}
