9196d5636fea27821e57cc97a8860de689be0e00
Julie Crandall <10697751@my.uvu.edu>
2020-01-28 23:45:29 -0700

    Rewrote flipping and vector3data. Figured out how to add more fonts into unity (I chose a lousy one), and began rewriting UItextcontrol, but taking time to study it.

---
+++ SpringSemester/Assets/Scripts/UITextCon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class UITextCon : MonoBehaviour
+{
+
+	private Text cornText;
+
+	private void Awake()
+	{
+		cornText = GetComponent<Text>();
+	}
+	//public void UpdateText ()
+}
