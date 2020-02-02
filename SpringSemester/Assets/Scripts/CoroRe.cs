4df22a2b71dc8f1fbeb41cbcd6e691b7fd4a8b46
Julie Crandall <10697751@my.uvu.edu>
2020-02-01 20:43:11 -0700

    Basic coroutines.

---
+++ SpringSemester/Assets/Scripts/CoroRe.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroRe : MonoBehaviour
+{
+
+	public UnityEvent waitHere, andAgain, thatsIt;
+	public float seconds = 1f;
+	public int counter = 3;
+	public bool beginRunin;
+	private WaitForSeconds wsObj;
+
+	private void Awake()
+	{
+		wsObj = new WaitForSeconds(seconds);
+	}
+
+	public void CallCourtine()
+	{
+		StartCoroutine(RunCorou());
+	}
+
+	IEnumerator RunCorou()
+	{
+		waitHere.Invoke();
+
+		while (counter > 0)
+		{
+			andAgain.Invoke();
+			yield return wsObj;
+			counter--;
+		}
+
+		while (beginRunin)
+		{
+			andAgain.Invoke();
+			yield return wsObj;
+			counter--;
+		}
+		thatsIt.Invoke();
+	}
+}
