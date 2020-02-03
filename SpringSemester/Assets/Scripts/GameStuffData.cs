1573ef7e7e0b03e20620a7ea3f39ab9a4b63e8bd
Julie Crandall <10697751@my.uvu.edu>
2020-02-02 23:57:32 -0700

    Working on playerdata, still figuring out exactly what I need for my game. -- thus commented out lines.

---
+++ SpringSemester/Assets/Scripts/GameStuffData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStuffData : MonoBehaviour
+{
+
+	public Sprite spriteArt;
+	public Color colorArt;
+	public GameObject prefab;
+}
---
+++ SpringSemester/Assets/Scripts/PlayerDataRe.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class PlayerDataRe : GameStuffData
+{
+
+	//public list<artData> art;
+
+	public UnityAction<GameObject> instanceAction;
+	public UnityEvent onStartEvent;
+
+	public FloatData health;
+	//public EquipData equipment;
+	//public HeadgearData headMagic;
+	//public PowerData powerslvl;
+
+	public void InstancePlayer()
+	{
+		var newPlayer = Instantiate(prefab);
+		//var newSprite = newPlayer.GetComponentInChildren<SpriteRenderer>;
+		//newSprite.sprite = spriteArt;
+		//newSprite.color = colorArt;
+		
+	}
+	
+}
