024652f1b7df1af85236c0659c4c19d16aea6cca
Julie Crandall <10697751@my.uvu.edu>
2020-02-08 21:18:29 -0700

    Put together new playerhandler.

---
+++ SpringSemester/Assets/Scripts/PlayerHand.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class PlayerHand : MonoBehaviour
+{
+
+	public PlayerDataRe playDataObj;
+
+	public void SwitchPlay(PlayerDataRe data)
+	{
+		playDataObj = data;
+		
+	}
+
+	public void OnRun()
+	{
+		playDataObj.Start();
+	}
+
+}
