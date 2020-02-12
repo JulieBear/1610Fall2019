8edf0877e04a3462ebc0f3bdf672bb3b23c39ea7
Julie Crandall <10697751@my.uvu.edu>
2020-02-11 23:20:13 -0700

    Prepping for AI testing, made the handler. Memorizing older scripts.

---
+++ SpringSemester/Assets/Scripts/AIHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIHandler : MonoBehaviour
+{
+
+	private Vector3 currDestination;
+	private NavMeshAgent theAgent;
+	private Vector3 starting;
+	public Transform destinObj;
+	
+	void Start ()
+	{
+		starting = transform.position;
+		currDestination = starting;
+		theAgent = GetComponent<NavMeshAgent>();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		currDestination = destinObj.position;
+	}
+	
+	//--return back to locations - starting = assign.
+
+	private void OnTriggerExit(Collider other)
+	{
+		currDestination = starting;
+	}
+
+	private void Update()
+	{
+		theAgent.destination = currDestination;
+	}
+}
+
+//update takes them back from current destination.
---
+++ SpringSemester/Assets/Scripts/PlayerHandMem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+//writing player handler again for memorization purposes.
+public class PlayerHandMem : MonoBehaviour
+{
+	public PlayerDataRe plDataObj;
+
+	public void PlaySwitch(PlayerDataRe dataRe)
+	{
+		plDataObj = dataRe;
+	}
+
+	public void onRun()
+	{
+		plDataObj.Start();
+	}
+}
