c682bd27705d8ebe279709498f77aebfc26d97f7
Julie Crandall <10697751@my.uvu.edu>
2020-02-13 21:54:21 -0700

    Help from Unity site, attempted to make my own remix of it, didn't work. Succeeded with theirs.

---
+++ SpringSemester/Assets/Scripts/CoroDestroyTwo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class CoroDestroyTwo : MonoBehaviour {
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("crystalStop"))
+		{
+			Destroy(other.gameObject);
+		}
+	}
+
+	IEnumerator destroyHoldEffect(GameObject particles)
+	{
+		yield return new WaitForSeconds(0.5f);
+		if (particles)
+		{
+			particles.GetComponent<ParticleSystem>().loop = false;
+		}
+		yield return new WaitForSeconds(1);
+		if (particles)
+		{
+			Destroy(particles);
+		}
+	}
+	
+	
+	//IEnumerator destroyHoldEffect(GameObject)
+	//{
+	//	yield return new WaitForSeconds(0.5f);
+	//	if (gameObject)
+	//	{
+			
+	//	}
+	//}
+}
