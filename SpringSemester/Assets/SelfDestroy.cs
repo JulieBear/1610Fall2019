ffb4eb55a81b2942601b8b1755f462c4d2fa2cd4
Julie Crandall <10697751@my.uvu.edu>
2020-02-16 23:30:39 -0700

    Quick rewrite with self destroy.

---
+++ SpringSemester/Assets/SelfDestroy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SelfDestroy : MonoBehaviour
+{
+
+	public float delay = 3f;
+	
+	void Start () {
+		Destroy(gameObject, delay);
+	}
+
+	public void DestroyOnImpact()
+	{
+		Destroy(gameObject);
+	}
+}
