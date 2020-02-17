5bb44f30138e70ce5100cf82594abd4a9b985b9a
Julie Crandall <10697751@my.uvu.edu>
2020-02-16 22:52:16 -0700

    Writing coroutine script again.

---
+++ SpringSemester/Assets/Scripts/CoroPA.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroPA : MonoBehaviour
+{
+
+	public UnityEvent eventAgain, startEvent, finishEvent;
+	public float seconds = 1f;
+	public int count = 3;
+	public bool startRun;
+	private WaitForSeconds wsObject;
+
+	void Awake () {
+		wsObject = new WaitForSeconds(seconds);
+	}
+
+	public void GetCoroutine()
+	{
+		StartCoroutine(RunCoroutine());
+	}
+
+	IEnumerator RunCoroutine()
+	{
+		Debug.Log(count);
+
+		while (count > 0)
+		{
+			eventAgain.Invoke();
+			yield return wsObject;
+			count--;
+		}
+
+		while (startRun)
+		{
+			eventAgain.Invoke();
+			yield return wsObject;
+			count--;
+		}
+		finishEvent.Invoke();
+	}
+	
+}
