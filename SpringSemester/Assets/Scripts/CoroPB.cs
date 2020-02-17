abb9d3275a0d5046ee0d3d0facd61af18d690b6b
Julie Crandall <10697751@my.uvu.edu>
2020-02-16 23:23:37 -0700

    Another coroutine. Still not quite grasping it.

---
+++ SpringSemester/Assets/Scripts/CoroPB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroPB : MonoBehaviour
+{
+
+	public bool running = true;
+	public int index = 3;
+	public float seconds = 1f;
+	public UnityEvent onTriggerEnter;
+
+	private WaitForSeconds secondsObj;
+
+	IEnumerator OnTriggerEnter(Collider other)
+	{
+		while (index > 0)
+		{
+			Debug.Log(index);
+			index--;
+			yield return new WaitForSeconds(1f);
+			onTriggerEnter.Invoke();
+		}	
+	}
+}
