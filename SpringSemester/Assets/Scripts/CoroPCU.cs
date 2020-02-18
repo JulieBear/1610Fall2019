04f63bad9158eae9896d119e74cd7588e65a0d0c
Julie Crandall <10697751@my.uvu.edu>
2020-02-17 22:24:04 -0700

    Tried out the Coroutine tutorial from Unity. Had to erase and redo due to red lines, still didn't get it to work.

---
+++ SpringSemester/Assets/Scripts/CoroPCU.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroPCU : MonoBehaviour
+{
+	public UnityEvent stopCoroutine;
+	public float smooth = 7f;
+
+	public static Vector3 Target
+	{
+		get { return toward; }
+		set
+		{
+			toward = value;
+			
+			print(message:"Move");
+			print("toward");
+		}
+	}
+
+	private static Vector3 toward;
+
+	IEnumerator Move(Vector3 toward)
+	{
+		while (Vector3.Distance(transform.position, b: toward) > 0.05f)
+		{
+			transform.position = Vector3.Lerp(transform.position, toward, smooth * Time.deltaTime);
+			yield return null;
+		}
+	}
+}
