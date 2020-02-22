33e2089a88a217f3c2771d0e427b2255f6afabd2
Julie Crandall <10697751@my.uvu.edu>
2020-02-21 21:36:36 -0700

    Made ANOTHER coroutine. Half works, half doesn't. Half excited success!!!

---
+++ SpringSemester/Assets/Scripts/CoroPD.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroPD : MonoBehaviour
+{
+
+	public int index = 5;
+	public UnityEvent onTriggerEnter, onTriggerExit;
+	public float secs = 1f;
+	public bool getGoing;
+
+	private WaitForSeconds secObj;
+
+	public void Awake()
+	{
+		secObj = new WaitForSeconds(secs);
+	}
+
+	public void CallCoroutine()
+	{
+		StartCoroutine(OnTriggerEnter(null));
+	}
+
+	IEnumerator OnTriggerEnter(Collider other)
+	{
+		onTriggerEnter.Invoke();
+		
+		while (index > 0)
+		{
+			Debug.Log(index);
+			index--;
+			yield return new WaitForSeconds(1f);
+			onTriggerEnter.Invoke();
+		}
+	}
+}
