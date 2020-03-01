98f54a26afeafc6df87ffef9baef5619af967b67
Julie Crandall <10697751@my.uvu.edu>
2020-02-29 21:55:22 -0700

    Got the text UI to work.

---
+++ SpringSemester/Assets/Scripts/UIimageCon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class UIimageCon : MonoBehaviour
+{
+
+    private Image img;
+
+    private void Awake()
+    {
+        img = GetComponent<Image>();
+    }
+    
+    
+
+}
--- SpringSemester/Assets/Scripts/imageBehaviour.cs
+++ SpringSemester/Assets/Scripts/imageBehaviour.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Image))]
 public class imageBehaviour : MonoBehaviour
 {
-
 	public Image img;
 	public FloatValue dataObj;
 
