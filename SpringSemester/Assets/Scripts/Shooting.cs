1cc067ed07a81760364e5fb77a75500b330ed8a7
Julie Crandall <10697751@my.uvu.edu>
2020-03-01 23:27:26 -0700

    Figured out how to make two objects disappear. Attempted a bullet script made by someone else. Didn't work, tried to fix, still didn't work.

---
+++ SpringSemester/Assets/Scripts/Shooting.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+public class Shooting : MonoBehaviour
+{
+    public bool facingRight;
+    public GameObject leftBull, rightBull;
+    private Transform firing;
+
+    private void Awake()
+    {
+        firing = transform.FindChild("Firing");
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        if (facingRight) {
+            Instantiate (rightBull, firing.position, Quaternion.identity);
+        }
+        if (!facingRight)
+        {
+            Instantiate (leftBull,firing.position, Quaternion.identity);
+        }
+    }
+    }
+    
+
