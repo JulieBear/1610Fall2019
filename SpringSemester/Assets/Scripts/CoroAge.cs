9eeeb6aa05c76dd75487ec0b637cc8ec422ab758
Julie Crandall <10697751@my.uvu.edu>
2020-03-03 23:50:37 -0700

    Half way figured out age coroutine, can't figure out how to stop coroutine mid coroutine.

---
+++ SpringSemester/Assets/Scripts/CoroAge.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroAge : MonoBehaviour
+{
+
+	public int num = 3;
+	public UnityEvent onTrigger, onTriggerE;
+	public float secs = 5f;
+	public float stopSecs = .5f;
+	public bool canRun;
+
+	private WaitForSeconds secObj;
+
+	private void Awake()
+	{
+		secObj = new WaitForSeconds(secs);
+	}
+	
+	public void CallCoroutine()
+	{
+		StartCoroutine(OnTriggerStay(null));
+	}
+	
+	IEnumerator OnTriggerStay(Collider other)
+	{
+		onTrigger.Invoke();
+
+		while (num > 0)
+		{
+			num--;
+			yield return new WaitForSeconds(secs);
+			onTrigger.Invoke();
+		}
+
+		while (canRun)
+		{
+			onTrigger.Invoke();
+			yield return new WaitForSeconds(stopSecs);
+			num--;
+		}
+	}
+
+}
