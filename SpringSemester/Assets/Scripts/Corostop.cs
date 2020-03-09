f9b2f0a192566f1d2079c710d02c08ba4414f62d
Julie Crandall <10697751@my.uvu.edu>
2020-03-08 23:26:16 -0600

    Figured how to stop coroutine, I think.

--- SpringSemester/Assets/Scripts/Corostop.cs
+++ SpringSemester/Assets/Scripts/Corostop.cs
@@ -1,15 +1,58 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Corostop : MonoBehaviour {
+public class Corostop : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
+	public int number = 3;
+	public UnityEvent onTriggerStay, onTriggerExit;
+	public float seconds = 5f;
+	public float stopSeconds = .5f;
+	public bool startRun;
+
+	private WaitForSeconds secondObj;
+
+
+	private void OnTriggerExit(Collider other)
+	{
+		StopCoroutine(onTriggerExit);
+	}
+
+	private void StopCoroutine(UnityEvent methodName)
+	{
+		onTriggerExit.Invoke();
+	}
+
+	private void Awake()
+	{
+		secondObj=new WaitForSeconds(seconds);
+	}
+
+	public void CallCoroutine()
+	{
+		StartCoroutine(OnTriggerStay(null));
+	}
+
+
+	
+	//public void StopCoroutine()
+	//{
+	//	StopCoroutine();
+	//}
+
+	IEnumerator OnTriggerStay(Collider other)
+	{
+		onTriggerStay.Invoke();
+		while (number > 0)
+		{
+			number--;
+			yield return new WaitForSeconds(seconds);
+			onTriggerStay.Invoke();
+		}
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
