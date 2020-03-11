bf3aa42fd8ef448d39ec023c696ef41772edc9eb
Julie Crandall <10697751@my.uvu.edu>
2020-03-10 22:47:03 -0600

    Attempt cinemachine...something went wrong.

---
+++ SpringSemester/Assets/Scripts/CinemachineCon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Cinemachine;
+using UnityEngine;
+
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class CinemachineCon : MonoBehaviour
+{
+
+    public PlayerDataRe playerData;
+    private CinemachineVirtualCamera virtCamera;
+
+    private void Awake()
+    {
+        virtCamera = GetComponent<CinemachineVirtualCamera>();
+        playerData.instanceAction = InstanceHandler;
+        playerData.InstancePlayer();
+    }
+
+    private void InstanceHandler(GameObject obj)
+    {
+        virtCamera.Follow = obj.transform;
+    }
+}
