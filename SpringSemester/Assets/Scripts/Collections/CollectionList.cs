510075741f5e794aa17d7375d1497639ea78c720
Julie Crandall <10697751@my.uvu.edu>
2020-03-11 23:25:10 -0600

    For each and lists.

---
+++ SpringSemester/Assets/Scripts/Collections/CollectionList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CollectionList : ScriptableObject
+{
+
+	public List<Collable> collectables;
+}
---
+++ SpringSemester/Assets/Scripts/ListsLoops.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ListsLoops : ScriptableObject
+{
+
+	public List<string> stringList;
+	public string oneString;
+
+	public void AddTolist(string stringObj)
+	{
+		stringList.Add(stringObj);
+	}
+
+	public void RemoveFromList(string stringObj)
+	{
+		for (int i = 0; i < stringList.Count; i++)
+		{
+			if (stringList[i] == stringObj)
+			{
+				stringList.Remove(stringObj);
+			}
+		}
+	}
+
+	public void SortList()
+	{
+		stringList.Sort();
+	}
+
+	public void CheckList()
+	{
+		foreach (var obj in stringList)
+		{
+			if (obj.Contains(oneString))
+			{
+				Debug.Log(obj);
+			}
+		}
+	}
+}
