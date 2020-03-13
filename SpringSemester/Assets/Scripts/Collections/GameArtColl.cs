a561aec0f4aa383994db4e7d96b7202b259d51a3
Julie Crandall <10697751@my.uvu.edu>
2020-03-12 23:29:45 -0600

    Created Weapon Art List for game.

---
+++ SpringSemester/Assets/Scripts/Collections/GameArtColl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class GameArtColl : ScriptableObject
+{
+
+	public List<ArtData> collList;
+
+	public void AddData(ArtData obj)
+	{
+		if (!collList.Contains(obj))
+		{
+			collList.Add(obj);
+		}
+	}
+
+	public void RemoveLastItem()
+	{
+		if (collList.Contains(null))
+		{
+			collList.RemoveAt(collList.Count-1);
+		}
+	}
+
+}
