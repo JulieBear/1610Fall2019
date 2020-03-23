d7ae06a6b4a7d1538424cf656115aafeba4a21e8
Julie Crandall <10697751@my.uvu.edu>
2020-03-23 10:47:48 -0600

    In class enums

---
+++ SpringSemester/Assets/Scripts/Enummy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+public class Enummy : MonoBehaviour {
+
+	public enum GameStates
+	{
+		Starting,
+		Playing,
+		InStore,
+		Pausing,
+		Ending
+	}
+
+	public GameStates currGameState;
+	
+	public enum PlayerStates
+	{
+		Idle,
+		Walk,
+		Run,
+		Jump
+	}
+
+	public PlayerStates currPlayerstate;
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () 
+	{
+		switch (currPlayerstate)
+		{
+			case PlayerStates.Idle:
+				print("I am idling.");
+				break;
+			case PlayerStates.Walk:
+				break;
+			case PlayerStates.Run:
+				break;
+			case PlayerStates.Jump:
+				break;
+			default:
+				throw new ArgumentOutOfRangeException();
+		}
+	}
+}
