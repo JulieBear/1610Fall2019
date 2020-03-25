4f80e7a8b350b6f8cccac3426bd6ff3f59951c6c
Julie Crandall <10697751@my.uvu.edu>
2020-03-24 23:48:13 -0600

    Played around with AI ideas.

---
+++ SpringSemester/Assets/Scripts/Collide.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Collide : MonoBehaviour
+{
+
+	public UnityEvent collide;
+
+	public void OnCollisionEnter(Collision other)
+	{
+		collide.Invoke();
+		
+	}
+}
