a86eb557f45bb924481fbc398f232b376400c9c7
Julie Crandall <10697751@my.uvu.edu>
2020-03-26 23:45:09 -0600

    AI scripting again.

---
+++ SpringSemester/Assets/Scenes/AIPrac/NavAgeBehav.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavAgeBehav : MonoBehaviour
+{
+
+	private NavMeshAgent agent;
+	//public Vector3 location;
+	public Transform player;
+	public float speed = 5f;
+
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+		agent.speed = speed;
+	}
+	
+	
+	void Update ()
+	{
+		agent.destination = player.position;
+	}
+}
