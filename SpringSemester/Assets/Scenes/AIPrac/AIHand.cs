b94c6b8c2d9fe2fcfc3b917b552147da862a514f
Julie Crandall <10697751@my.uvu.edu>
2020-03-28 23:43:50 -0600

    Trying to make my bullets do things.

---
+++ SpringSemester/Assets/Scenes/AIPrac/AIHand.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIHand : MonoBehaviour
+{
+
+	private Vector3 currentDestination;
+	private NavMeshAgent thisAgent;
+	private Vector3 onStart;
+	public Transform destinationObj;
+
+	void Start ()
+	{
+		onStart = transform.position;
+		//destinationObj = onStart;
+		thisAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	
+	
+	
+	
+	void Update ()
+	{
+		thisAgent.destination = currentDestination;
+	}
+}
