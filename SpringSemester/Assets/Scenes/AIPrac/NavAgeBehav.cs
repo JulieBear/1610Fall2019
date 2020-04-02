aac0ec22870cb24ce1052fb2fccbf35efba988a5
Julie Crandall <10697751@my.uvu.edu>
2020-04-01 18:48:12 -0600

    In class AI and Patrolling.

--- SpringSemester/Assets/Scenes/AIPrac/NavAgeBehav.cs
+++ SpringSemester/Assets/Scenes/AIPrac/NavAgeBehav.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
@@ -9,16 +11,43 @@ public class NavAgeBehav : MonoBehaviour
 	//public Vector3 location;
 	public Transform player;
 	public float speed = 5f;
+	private Transform currDestination;
+	private int i;
+	private bool canHunt;
+
+	public List<Transform> patrolPoints;
 
 	void Start ()
 	{
 		agent = GetComponent<NavMeshAgent>();
 		agent.speed = speed;
+		currDestination = transform;
 	}
-	
-	
+
+	private void OnTriggerEnter(Collider other)
+	{
+		canHunt = true;
+		currDestination = player;
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		canHunt = false;
+		currDestination = transform;
+	}
+
 	void Update ()
 	{
-		agent.destination = player.position;
+		if (canHunt)
+		{
+			agent.destination = currDestination.position;
+			return;
+		}
+		
+		if (!agent.pathPending && agent.remainingDistance < 0.5f)
+		{
+			agent.destination = patrolPoints[i].position;
+			i = (i + 1) % patrolPoints.Count;
+		}
 	}
 }
---
+++ SpringSemester/Assets/Scripts/CorouCl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CorouCl : MonoBehaviour
+{
+	private WaitForFixedUpdate waitObj;
+	public bool canRun = true;
+	
+	void Start () {
+		waitObj = new WaitForFixedUpdate();
+	}
+
+	public IEnumerator OnStartCorou()
+	{
+		while (canRun)
+		{
+			yield return waitObj;
+			print("Repeating");
+		}
+	}
+}
