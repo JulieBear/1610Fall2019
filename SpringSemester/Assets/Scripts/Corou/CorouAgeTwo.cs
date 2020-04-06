f3f5c66f416fddfcc0b6216543f761bdca754e4f
Julie Crandall <10697751@my.uvu.edu>
2020-04-05 23:40:44 -0600

    Another - Coroutine - not successful, getting closer though.

---
+++ SpringSemester/Assets/Scripts/Corou/CorouAgeTwo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CorouAgeTwo : MonoBehaviour
+{
+
+	public int count = 5;
+	public float secon = 1f;
+	public IntDataObj numData;
+
+	public UnityEvent ageUp, ageStop;
+
+	IEnumerator OnTriggerEnter(Collider other)
+	{
+
+		while (count > 0)
+		{
+			yield return new WaitForSeconds(secon);
+			ageUp.Invoke();
+			count--;
+		}
+		
+		yield return new WaitForSeconds(secon);
+		ageStop.Invoke();
+	}
+	
+}
