6bacfa42cfa860dadc83dc44cd98ae1960190bc2
Julie Crandall <10697751@my.uvu.edu>
2020-04-08 22:57:47 -0600

    Flipping class.

--- SpringSemester/Assets/Scripts/FlippingRe.cs
+++ SpringSemester/Assets/Scripts/FlippingRe.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 
 public class FlippingRe : MonoBehaviour
 {
+	public KeyCode key1 = KeyCode.LeftArrow, key2 = KeyCode.RightArrow;
+	public float dir1 = 0, dir2 = 100;
 	public VectorThreeD threeD;
 	void Update () 
 	{
-		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		if (Input.GetKeyDown(key1))
 		{
-			transform.rotation = Quaternion.Euler(0,180,0);
+			transform.rotation = Quaternion.Euler(0,dir1,0);
 			threeD.val.x = -1;
 		}
 
-		if (Input.GetKeyDown(KeyCode.RightArrow))
+		if (Input.GetKeyDown(key2))
 		{
-			transform.rotation = Quaternion.Euler(0,0,0);
+			transform.rotation = Quaternion.Euler(0,dir2,0);
 			threeD.val.x = 1;
 		}
 	}
