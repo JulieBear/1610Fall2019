367016f6a27849c7d35aaf01c5dd2552e5b39705
Julie Crandall <10697751@my.uvu.edu>
2020-04-09 23:51:20 -0600

    Still trying to get bullets to recognize target. Redid scripts.

---
+++ SpringSemester/Assets/GunThree.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class GunThree : MonoBehaviour
+{
+
+	public Transform firePoint;
+	public GameObject bulletPre;
+	public float maxBullets = 3;
+	private int bullCount;
+
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Shooting();
+		}
+	}
+
+	void Shooting()
+	{
+		Instantiate(bulletPre, firePoint.position, firePoint.rotation);
+	}
+}
---
+++ SpringSemester/Assets/Scripts/Bullet1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class Bullet1 : MonoBehaviour
+{
+
+	public float speed = 10;
+	public int damage = 20;
+	public Rigidbody2D ridgey;
+	
+	
+	void Start ()
+	{
+		ridgey.velocity = transform.right * speed;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Ene enemy = other.GetComponent<Ene>();
+		if (enemy != null)
+		{
+			enemy.Damaging(damage);
+		}
+		Destroy(gameObject);
+	}
+}
