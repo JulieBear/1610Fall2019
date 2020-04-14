84ccc0fe1163c430f5d1eb4d1201f4146eb9862d
Julie Crandall <10697751@my.uvu.edu>
2020-04-13 23:39:31 -0600

    Layers - trying to figure out the corou age.

---
+++ SpringSemester/Assets/Scripts/Corou/CorouAgeTwo1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CorouAgeTwo1 : MonoBehaviour
+{
+
+	public int couer = 3;
+	public float onds = 1f;
+	public string mess = "try";
+	public IntDataObj numDat;
+
+	public UnityEvent onTrigEnt, onTrigEx;
+
+	IEnumerator OnTriggerEnter(Collider other)
+	{
+		onTrigEnt.Invoke();
+		while (couer>0)
+		{
+			numDat.value = couer;
+			yield return new WaitForSeconds(onds);
+			couer--;
+		}
+	}
+
+	IEnumerator OnTriggerExit(Collider other)
+	{
+		onTrigEx.Invoke();
+
+		while (couer > 0)
+		{
+			numDat.value = couer;
+			yield return new WaitForSeconds(onds);
+			couer--;
+		}
+	}
+}
