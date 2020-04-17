428404bd65557107a92fa5abd7fbb7a2c8855a5b
Julie Crandall <10697751@my.uvu.edu>
2020-04-16 23:21:06 -0600

    Another attempt at aging coroutine.

---
+++ SpringSemester/Assets/Scripts/Aging.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Aging : MonoBehaviour
+{
+	private WaitForFixedUpdate waitObj;
+	public int counter = 3;
+	public float seconds = 1f;
+	public IntDataObj numData;
+	public bool canRun = true;
+
+	public UnityEvent ontrigEnt;
+
+	private void Awake()
+	{
+		waitObj = new WaitForFixedUpdate();
+	}
+
+	IEnumerator OnTriggerEnter(Collider other)
+	{
+		while (canRun)
+		{
+			yield return waitObj;
+			ontrigEnt.Invoke();
+		}
+	}
+}
