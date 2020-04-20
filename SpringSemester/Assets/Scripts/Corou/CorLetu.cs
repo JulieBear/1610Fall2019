63a17cdf77e85a196669a72292e93a947bb3d263
Julie Crandall <10697751@my.uvu.edu>
2020-04-19 23:53:00 -0600

    Corou Age.

---
+++ SpringSemester/Assets/Scripts/Corou/CorLetu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class CorLetu : MonoBehaviour
+{
+
+	public float secs = 1f;
+	public IntDataObj numData;
+	public float stopSecs = .5f;
+	public bool canRun;
+	private WaitForSeconds secObj;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		secObj = new WaitForSeconds(secs);
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		secObj = new WaitForSeconds(stopSecs);
+	}
+
+	void Update () {
+		
+	}
+}
