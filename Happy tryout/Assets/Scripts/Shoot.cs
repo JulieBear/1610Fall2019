bab37fef0f4b36cad2af56c63f36b080218a1686
Julie Crandall <10697751@my.uvu.edu>
2020-04-28 03:26:08 -0600

    Gun - shooting.

---
+++ Happy tryout/Assets/Scripts/Shoot.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public bool facingRight;
+    public GameObject leftBull, rightBull;
+    private Transform firing;
+
+    private void Awake()
+    {
+        firing = transform.Find("Firing");
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        if (facingRight) {
+            Instantiate (rightBull, firing.position, Quaternion.identity);
+        }
+        if (!facingRight)
+        {
+            Instantiate (leftBull,firing.position, Quaternion.identity);
+        }
+    }
+}
